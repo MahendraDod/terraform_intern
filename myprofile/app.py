b5286431fd1be0fa40d46119a042ac25e89f745b
MahendraDod <101705872+MahendraDod@users.noreply.github.com>
2022-06-18 19:26:41 +0530

    Update app.py

--- myprofile/app.py
+++ myprofile/app.py
@@ -2,70 +2,25 @@ from flask import Flask,render_template, request
 from flask_mysqldb import MySQL
 import boto3
 import json
-import base64
-from botocore.exceptions import ClientError
-
-
 
 app = Flask(__name__)
 
-def get_secret():
+client = boto3.client('secretsmanager')
+response = client.get_secret_value(
 
-    secret_name = "arn:aws:secretsmanager:ap-south-1:229419693179:secret:prod/mydb/mydatabase-iiMwy9"
-    region_name = "ap-south-1"
+    SecretId='prod/mydb/mydatabase'
 
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
-    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    # We rethrow the exception by default.
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'DecryptionFailureException':
-            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-            # An error occurred on the server side.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            # You provided an invalid value for a parameter.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            # You provided a parameter value that is not valid for the current state of the resource.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-            # We can't find the resource that you asked for.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-    else:
-        # Decrypts secret using the associated KMS key.
-        # Depending on whether the secret is a string or binary, one of these fields will be populated.
-        if 'SecretString' in get_secret_value_response:
-            secret = get_secret_value_response['SecretString']
-        else:
-            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+)
+secretDict = json.loads(response['SecretString'])
 
  
-app.config['MYSQL_HOST'] = decoded_binary_secret['host']
+app.config['MYSQL_HOST'] = secretDict['host']
 
-app.config['MYSQL_USER'] = decoded_binary_secret['username']
+app.config['MYSQL_USER'] = secretDict['username']
   
-app.config['MYSQL_PASSWORD'] = decoded_binary_secret['password']
+app.config['MYSQL_PASSWORD'] = secretDict['password']
  
-app.config['MYSQL_DB'] = decoded_binary_secret['dbname']
+app.config['MYSQL_DB'] = secretDict['dbname']
  
 mysql = MySQL(app)
  

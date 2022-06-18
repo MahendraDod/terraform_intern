56e6c6b5ad103a608cf52876c82db50f0e264bd8
MahendraDod <101705872+MahendraDod@users.noreply.github.com>
2022-06-18 17:07:42 +0530

    Create app.py

---
+++ myprofile/app.py
@@ -0,0 +1,45 @@
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
+import boto3
+import json
+
+app = Flask(__name__)
+
+client = boto3.client('secretsmanager')
+response = client.get_secret_value(
+
+    SecretId='prod/mysql/mydatabase'
+
+)
+secretDict = json.loads(response['SecretString'])
+
+ 
+app.config['MYSQL_HOST'] = secretDict['host'],
+
+app.config['MYSQL_USER'] = secretDict['username'],
+  
+app.config['MYSQL_PASSWORD'] = secretDict['password'],
+ 
+app.config['MYSQL_DB'] = secretDict['dbname']
+ 
+mysql = MySQL(app)
+ 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+ 
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return "Login via the login Form"
+     
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        cursor = mysql.connection.cursor()
+        cursor.execute(''' INSERT INTO info_table VALUES(%s,%s)''',(name,age))
+        mysql.connection.commit()
+        cursor.close()
+        return f"Done!!"
+ 
+app.run(host='localhost', port=5000)
